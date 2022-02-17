b47c36c713c97b743fa80d3442cd415f7cdc8009
Stift007 <dsstift@icloud.com>
2022-02-17 18:38:26 +0100

    Restructured stuff

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='botlistpy',
+  version='0.0.1',
+  description='An async wrapper around BotList.me\'s API',
+  long_description=open('README.md').read(),
+  url='',  
+  author='DS_Stift007',
+  author_email='dsstift@icloud.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='botlist api wrapper', 
+  packages=find_packages(),
+  install_requires=['aiohttp','requests'] 
+)
