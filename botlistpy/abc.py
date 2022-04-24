0cf7bb6d7490b4896f8fd971a8fdc95d8470665c
Stift007 <74728802+Stift007@users.noreply.github.com>
2022-04-24 13:29:13 +0200

    Update abc.py

--- botlistpy/abc.py
+++ botlistpy/abc.py
@@ -1,24 +1,42 @@
-class BotlistAPIResponse:
-    async def __init__(self,raw=None) -> None:
-        if hasattr(raw,"status_code"):
-            self.status_code = raw.status_code
-        else:
-            self.status_code = raw.status
+import typing
+
+class SyncBotlistAPIResponse:
+    def __init__(self,raw=None) -> None:
+        self.status_code = raw.status_code
         self.content = raw.content
         self.res = raw
         self.json = raw.json()
         self.success = self.status_code < 400
 
-class Asset:
-    def __init__(self,bytedata):
-        self.bytes = bytedata
+
+class ItemStruct:
+    def __init__(self):
+        self.items = {}
+
+    def from_object(self,obj:typing.Any):
+        for item in list(obj.__dict__.keys()):
+          if not item.startswith("__"):
+            self.items[item] = obj.__dict__[item]
+
+    @classmethod
+    def from_json(cls,json):
+        new = cls()
+        for item in list(json.keys()):
+            new[item] = json[item]
+
+        return new
+
+
+    def __getitem__(self,item):
+        return self.items.get(item)
         
-    def save(self,file):
-        open(file,"wb").write(self.bytes)
-        
-class SyncBotlistAPIResponse:
+    def __setitem__(self,item,value):
+        self.items[item] = value
+
+
+class BotlistAPIResponse:
     def __init__(self,raw=None) -> None:
-        self.status_code = raw.status_code
+        self.status_code = raw.status
         self.content = raw.content
         self.res = raw
         self.success = self.status_code < 400
@@ -43,6 +61,16 @@ class BotlistBaseUser:
     def __str__(self):
         return self.__dict__
 
+class Asset:
+    def __init__(self,byte_data):
+        self.bytes = byte_data
+        
+    def save(self,fp):
+        with open(fp,"wb") as f:
+            f.write(self.bytes)
+
+
+    
 class Bot(BotlistBaseUser):
     def __init__(self,id,**kwargs):
         super().__init__(id,kwargs)
